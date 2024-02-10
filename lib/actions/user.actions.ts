e21d909e10e8016abbf3bdb154412650fc5fba01
mtake986 <tosa9629@email.com>
2024-02-09 17:36:41 -0700

    connected MongoDB, create/update user

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,63 @@
+// they can be defined inside server components and/or called from Client components.
+"use server";
+
+import { revalidatePath } from "next/cache";
+import User from "../models/user.model";
+import { connectToDB } from "../mongoose";
+
+type Params = {
+  userId: string;
+  username: string;
+  name: string;
+  bio: string;
+  image: string;
+  path: string;
+};
+
+export async function updateUser({
+  userId,
+  username,
+  name,
+  bio,
+  image,
+  path,
+}: Params): Promise<void> {
+  connectToDB();
+
+  try {
+    console.log("=== here is the uploading info: ",
+      { id: userId },
+      {
+        username: username.toLowerCase(),
+        name,
+        bio,
+        image,
+        onvoarded: true,
+      },
+      {
+        upsert: true,
+      }, "==========="
+    );
+    await User.findOneAndUpdate(
+      { id: userId },
+      {
+        username: username.toLowerCase(),
+        name,
+        bio,
+        image,
+        onvoarded: true,
+      },
+      {
+        upsert: true,
+      }
+    );
+
+    console.log("User.findOneAndUpdate is done");
+
+    if (path === "/profile/edit") {
+      revalidatePath(path);
+    }
+  } catch (error: any) {
+    throw new Error(`Failed to create/upate user ${error.message}`);
+  }
+}
